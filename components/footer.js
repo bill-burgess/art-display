fb86d70e70c0c4d5c5e969c8253645d382dd0b9d
Bill Burgess <wmhburgess@gmail.com>
2017-08-13 14:15:33 +1200

    adds basic footer

---
+++ components/footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+class Footer extends Component {
+
+  render () {
+
+    return (
+      <div
+        id='footer'
+        className='footer'
+      >
+      <div id='copyright'>
+        Copyright ©2017 Chloe Summerhayes
+      </div>
+      </div>
+    )
+  }
+
+}
+
+module.exports = Footer
